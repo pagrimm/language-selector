d7a1ca399b6dcd3ce95011dbc357f9253ae56630
Peter Grimm <pagrimm@gmail.com>
2020-06-05 12:34:37 -0700

    add skeleton scoring logic to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
-function score(value) {
-  if (value === 1) {
-    option1 =+
+function score(input) {
+  if (input === 1) {
+    jsResults +=;
+  }
+  if (input === 2) {
+    pythonResults +=;
+  }
+  if (input === 3) {
+    cResults +=;
+  }
+}
+function reveal(jsResults, pythonResults, cResults) {
+  $("section.results").hide();
+  if (jsResults > pythonResults && jsResults > cResults) {
+    $("section#javascript").show();
+  }
+  else if (pythonResults > jsResults && pythonResults > cResults) {
+    $("section#python").show();
+  }
+  else if (cResults > jsResults && cResults > cResults) {
+    $("section#c").show();
   }
 }
