65eb8903b50b931cf796679346db10479a6eadf2
Peter Grimm <pagrimm@gmail.com>
2020-06-07 14:40:58 -0700

    clean up js formatting

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function getNameInputs(){
     $("input#question1").val()
   ];
   return name;
-}
+};
 
 //Function to get inputs from the scored fields
 function getScoreInputs() {
@@ -17,7 +17,7 @@ function getScoreInputs() {
     parseInt($("input[name='question5']:checked").val())
   ];
   return scores;
-}
+};
 
 //Function to add up scores and determine the winner
 function evalWinner(scores) {
@@ -28,17 +28,19 @@ function evalWinner(scores) {
     {"name":"javascript","score":0},
     {"name":"python","score":0},
     {"name":"c","score":0},
-  ]
+  ];
   //loop through scores array, add up total scores
   for (i = 0; i < scores.length; i++) {
     if (scores[i] === 1) {
       totals[0].score++;
-    } else if (scores[i] === 2) {
+    }
+    else if (scores[i] === 2) {
       totals[1].score++;
-    } else if (scores[i] === 3) {
+    } 
+    else if (scores[i] === 3) {
       totals[2].score++;
     }
-  }
+  };
   //finds the highest score value in the array of objects
   highScore = Math.max(...totals.map(total => total.score));
   //filter the array of objects to find any objects with score values that equal the high score
@@ -46,7 +48,7 @@ function evalWinner(scores) {
   //convert the name values of winning object/objects to an array
   winner = winnerObjs.map(({ name }) => name); 
   return winner;
-}
+};
 
 //function to reveal the winning section
 function revealWinner(winner) {
@@ -61,14 +63,14 @@ function revealWinner(winner) {
     $("section#" + winner[0]).addClass("active");
     $("section#" + winner[1]).show();
   }
-}
+};
 
 //function to add name to the results title, add favorite color to name
 function addName(name) {
   $(".result-title").prepend("<span class=\"name-target\"></span>");
   $(".name-target").text(name[0]);
   $(".name-target").css("color", name[1]);
-}
+};
 
 //function to clear all inputs, reset radio buttons to default positions
 function resetInputs() {
@@ -77,7 +79,7 @@ function resetInputs() {
   $("input[type=radio]").prop("checked", false);
   $("input.default-radio").prop("checked", true);
   $("input[type=color]").val("#62c462");
-}
+};
 
 //function to reset results to hidden
 function resetResults(){
@@ -85,7 +87,7 @@ function resetResults(){
   $("section.results").removeClass("active");
   $("h3#tie").hide();
   $(".name-target").remove();
-}
+};
 
 //USER LOGIC
 //function for user interface, submit/reset button, calls other functions
